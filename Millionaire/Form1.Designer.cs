6512679c9d65c0421954e391a373978bfcc808ae
Daria <dashakukuruza86@gmail.com>
2023-04-15 00:53:54 +0300

    Add project files.

---
+++ Millionaire/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Millionaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.администраторскийРежимToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.Navy;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 50;
+            this.listBox1.Location = new System.Drawing.Point(974, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 654);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Image = global::Millionaire.Properties.Resources.Снимок_экрана_2023_04_14_114334;
+            this.button1.Location = new System.Drawing.Point(974, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 66);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Image = global::Millionaire.Properties.Resources.Снимок_экрана_2023_04_14_113832;
+            this.button2.Location = new System.Drawing.Point(1073, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 66);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Image = global::Millionaire.Properties.Resources.Снимок_экрана_2023_04_14_114038;
+            this.button3.Location = new System.Drawing.Point(1172, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 66);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::Millionaire.Properties.Resources.imgonline_com_ua_Replace_color_5cVMtO5oE9__2___1___1___3_;
+            this.button6.Location = new System.Drawing.Point(108, 31);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 52);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Navy;
+            this.button7.Font = new System.Drawing.Font("Georgia", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(204, 31);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 52);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Стоп";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.Image = global::Millionaire.Properties.Resources.Снимок_экрана_2023_04_14_224904__1__bmp;
+            this.button8.Location = new System.Drawing.Point(12, 31);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 52);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Navy;
+            this.textBox1.Location = new System.Drawing.Point(12, 432);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(943, 144);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.BackColor = System.Drawing.Color.Navy;
+            this.button4.Location = new System.Drawing.Point(12, 594);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(465, 77);
+            this.button4.TabIndex = 10;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.Navy;
+            this.button5.Location = new System.Drawing.Point(490, 594);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(465, 77);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button9.BackColor = System.Drawing.Color.Navy;
+            this.button9.Location = new System.Drawing.Point(12, 688);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(465, 77);
+            this.button9.TabIndex = 12;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button10.BackColor = System.Drawing.Color.Navy;
+            this.button10.Location = new System.Drawing.Point(490, 688);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(465, 77);
+            this.button10.TabIndex = 13;
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.администраторскийРежимToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1270, 30);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // администраторскийРежимToolStripMenuItem
+            // 
+            this.администраторскийРежимToolStripMenuItem.Name = "администраторскийРежимToolStripMenuItem";
+            this.администраторскийРежимToolStripMenuItem.Size = new System.Drawing.Size(217, 24);
+            this.администраторскийРежимToolStripMenuItem.Text = "Администраторский режим";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = global::Millionaire.Properties.Resources.my__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1270, 786);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Кто хочет стать миллионером";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem администраторскийРежимToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
+
---
+++ Millionaire/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Millionaire
+{
+    public partial class Form1 : Form
+    {
+        private List<Question> questions = new List<Question>();
+        private List<Price> prices = new List<Price>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadQuestions();
+        }
+
+        private void LoadQuestions()
+        {
+            using (TextFieldParser parser = new TextFieldParser("Questions.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    int questionNumber;
+                    if (!int.TryParse(fields[0], out questionNumber))
+                    {
+                        MessageBox.Show("Error: Invalid question number format");
+                        continue;
+                    }
+
+                    int questionPrice;
+                    if (!int.TryParse(fields[1].Trim(), out questionPrice))
+                    {
+                        MessageBox.Show("Error: Invalid question price format");
+                        continue;
+                    }
+
+                    string questionText = fields[2];
+                    string[] answers = fields.Skip(3).ToArray();
+                    Question question = new Question
+                    {
+                        Number = questionNumber,
+                        Text = questionText,
+                        Answers = answers
+                    };
+
+                    Price price = new Price
+                    {
+                        Value = questionPrice
+                    };
+
+                    questions.Add(question);
+                    prices.Add(price);
+
+                    listBox1.Items.Add($"{questionNumber} - {questionPrice} грн");
+                }
+            }
+        }
+
+        public class Question
+        {
+            public int Number { get; set; }
+            public string Text { get; set; }
+            public string[] Answers { get; set; }
+        }
+
+        public class Price
+        {
+            public int Value { get; set; }
+        }
+    }
+}
