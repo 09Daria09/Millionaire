3aee8372b600de332fc747427a5d21bc31499391
Daria <dashakukuruza86@gmail.com>
2023-04-15 23:14:51 +0300

    add

--- Millionaire/Form1.Designer.cs
+++ Millionaire/Form1.Designer.cs
@@ -51,7 +51,7 @@
             // 
             this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.BackColor = System.Drawing.Color.Navy;
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.listBox1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
             this.listBox1.FormattingEnabled = true;
@@ -70,6 +70,7 @@
             this.button1.Size = new System.Drawing.Size(86, 66);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -99,6 +100,7 @@
             this.button6.Size = new System.Drawing.Size(90, 52);
             this.button6.TabIndex = 6;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -120,59 +122,73 @@
             this.button8.Size = new System.Drawing.Size(90, 52);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.Color.Navy;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
             this.textBox1.Location = new System.Drawing.Point(12, 432);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(943, 144);
             this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button4
             // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.button4.BackColor = System.Drawing.Color.Navy;
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Location = new System.Drawing.Point(12, 594);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(465, 77);
             this.button4.TabIndex = 10;
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button5.BackColor = System.Drawing.Color.Navy;
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.Color.White;
             this.button5.Location = new System.Drawing.Point(490, 594);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(465, 77);
             this.button5.TabIndex = 11;
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button4_Click);
             // 
             // button9
             // 
-            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button9.BackColor = System.Drawing.Color.Navy;
+            this.button9.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.button9.ForeColor = System.Drawing.Color.White;
             this.button9.Location = new System.Drawing.Point(12, 688);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(465, 77);
             this.button9.TabIndex = 12;
             this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button4_Click);
             // 
             // button10
             // 
-            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button10.BackColor = System.Drawing.Color.Navy;
+            this.button10.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button10.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.button10.ForeColor = System.Drawing.Color.White;
             this.button10.Location = new System.Drawing.Point(490, 688);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(465, 77);
             this.button10.TabIndex = 13;
             this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button4_Click);
             // 
             // menuStrip1
             // 
@@ -183,7 +199,7 @@
             this.оПрограммеToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1270, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(1270, 28);
             this.menuStrip1.TabIndex = 14;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -192,6 +208,7 @@
             this.играToolStripMenuItem.Name = "играToolStripMenuItem";
             this.играToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
             this.играToolStripMenuItem.Text = "Игра";
+            this.играToolStripMenuItem.Click += new System.EventHandler(this.играToolStripMenuItem_Click);
             // 
             // администраторскийРежимToolStripMenuItem
             // 
@@ -204,6 +221,7 @@
             this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
             this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
             this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
             // Form1
             // 
--- Millionaire/Form1.cs
+++ Millionaire/Form1.cs
@@ -1,33 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Media;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+//using System.Threading;
 using System.Windows.Forms;
-using System.Windows.Media;
 using Microsoft.VisualBasic.FileIO;
-using Label = System.Windows.Controls.Label;
-using ListBox = System.Windows.Forms.ListBox;
-
 namespace Millionaire
 {
     public partial class Form1 : Form
     {
         private List<Question> questions = new List<Question>();
         private List<Price> prices = new List<Price>();
+        int NumberQuestion = 1;
+        int NumerSelected = 14;
 
+        SoundPlayer playerBegin;
+        SoundPlayer playerTrue;
+        SoundPlayer playerFalse;
+
+        private Timer timer;
+        private int count = 0;
         public Form1()
         {
             InitializeComponent();
             LoadQuestions();
-        }
+            ReadFile();
+            listBox1.SelectedIndex = 14;
+            playerFalse = new SoundPlayer("false.wav");
+            playerTrue = new SoundPlayer("true.wav");
+            playerBegin = new SoundPlayer("begin.wav");
+            playerBegin.Play();
+            // System.Threading.Thread.Sleep(7000); // остановка выполнения программы на 5 секунд
 
+
+        }
+        private void ReadFile()
+        {
+            using (var reader = new StreamReader("Questions.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    if (values[0] == NumberQuestion.ToString())
+                    {
+                        string question = values[2].Replace("\"", "").Replace("^", "");
+                        string[] answers = new string[] { values[3].Replace("\"", "").Replace("^", ""), values[4].Replace("\"", "").Replace("^", ""), values[5].Replace("\"", "").Replace("^", ""), values[6].Replace("\"", "").Replace("^", "") };
+
+                        textBox1.Text = question;
+                        if (answers.Length > 0) button4.Text = answers[0];
+                        if (answers.Length > 1) button5.Text = answers[1];
+                        if (answers.Length > 2) button9.Text = answers[2];
+                        if (answers.Length > 3) button10.Text = answers[3];
+                    }
+                }
+            }
+        }
         private void LoadQuestions()
         {
             using (TextFieldParser parser = new TextFieldParser("Questions.csv"))
@@ -88,6 +119,164 @@ namespace Millionaire
         public class Price
         {
             public int Value { get; set; }
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            playerBegin.Stop();
+            Button clickedButton = (Button)sender;
+            string answerRight = null;
+            using (var reader = new StreamReader("Questions.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    if (values[0] == NumberQuestion.ToString())
+                    {
+                        string[] answers = new string[] { values[3], values[4], values[5], values[6] };
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            char[] charArray = answers[i].ToCharArray();
+                            if (charArray[0] == '^')
+                            {
+                                answerRight = answers[i].Replace("\"", "").Replace("^", "");
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (answerRight != clickedButton.Text)
+            {
+                playerFalse.Play();
+                await BlinkButton(clickedButton, Color.Red);
+                MessageBox.Show("Вы проиграли, но не расстраивайтесь. (╯︵╰,)\n" +
+                    "Продолжайте играть и тренироваться, и вы обязательно достигнете успеха!");
+                Application.Exit();
+            }
+            else
+            {
+                playerTrue.Play();
+            }
+
+            await BlinkButton(clickedButton, Color.Green);
+
+            NumberQuestion++;
+            NumerSelected--;
+            listBox1.SelectedIndex = NumerSelected;
+            ReadFile();
+        }
+
+        private async Task
+        BlinkButton(Button button, Color blinkColor)
+        {
+            int count = 0;
+            Color originalColor = button.BackColor;
+
+            while (count < 3)
+            {
+                button.BackColor = blinkColor;
+                await Task.Delay(500);
+                button.BackColor = originalColor;
+                await Task.Delay(500);
+                count++;
+            }
+        }
+        private void играToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Добро пожаловать в бесплатную онлайн-версию знаменитого игрового шоу “Кто хочет стать миллионером?”. " +
+                "В этой уникальной игре можно ощутить себя настоящим участником телепрограммы, завоевавшей миллионы поклонников по всему миру. " +
+                "Бесплатная версия игры позволит в режиме реального времени стать главным героем нашумевшей передачи.");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            string answerRight = null;
+            using (var reader = new StreamReader("Questions.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    if (values[0] == NumberQuestion.ToString())
+                    {
+                        string[] answers = new string[] { values[3], values[4], values[5], values[6] };
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            char[] charArray = answers[i].ToCharArray();
+                            if (charArray[0] == '^')
+                            {
+                                answerRight = answers[i].Replace("\"", "").Replace("^", "");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Random random = new Random();
+            int twoButt = 0;
+            while (twoButt < 3)
+            {
+                int numToDisable = random.Next(1, 4);
+                switch (numToDisable)
+                {
+                    case 1:
+                        if(answerRight != button4.Text)
+                        {
+                            button4.Enabled = false;
+                            twoButt++;
+                        }
+                        break;
+                    case 2:
+                        if (answerRight != button5.Text)
+                        {
+                            button5.Enabled = false;
+                            twoButt++;
+                        }
+                        break;
+                    case 3:
+                        if (answerRight != button9.Text)
+                        {
+                            button9.Enabled = false;
+                            twoButt++;
+                        }
+                        break;
+                    case 4:
+                        if (answerRight != button10.Text)
+                        {
+                            button10.Enabled = false;
+                            twoButt++;
+                        }
+                        break;
+                }
+
+
+            }
+
+
         }
     }
 }
